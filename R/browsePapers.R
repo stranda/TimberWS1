e76302b857f5bc28cf7d5a9c0a8aa6e96fb39887
allan strand <stranda@gmail.com>
2019-07-19 11:09:57 -0400

    literature browser

---
+++ R/browsePapers.R
@@ -0,0 +1,9 @@
+#' Setup a page to browse papers
+#'
+#' run with no parameters, it sets up a page that links to literature supplied for the workshop
+#'@export
+#'
+browsePapers <- function()
+{
+
+}
